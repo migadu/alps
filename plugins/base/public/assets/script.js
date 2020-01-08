d745f98bb7e77e9a1841ca9f5b7c9a6046c3b794
Simon Ser <contact@emersion.fr>
2020-01-08 12:38:09 +0100

    Add polyfill for browsers not supporting srcdoc

--- plugins/base/public/assets/script.js
+++ plugins/base/public/assets/script.js
@@ -1,8 +1,18 @@
 var emailFrame = document.getElementById("email-frame");
 if (emailFrame) {
+	// Resize the frame with its content
 	var resizeFrame = function() {
 		emailFrame.style.height = emailFrame.contentWindow.document.documentElement.scrollHeight + "px";
 	};
 	emailFrame.addEventListener("load", resizeFrame);
 	emailFrame.contentWindow.addEventListener("resize", resizeFrame);
+
+	// Polyfill in case the srcdoc attribute isn't supported
+	if (!emailFrame.srcdoc) {
+		var srcdoc = emailFrame.getAttribute("srcdoc");
+		var doc = emailFrame.contentWindow.document;
+		doc.open();
+		doc.write(srcdoc);
+		doc.close();
+	}
 }
