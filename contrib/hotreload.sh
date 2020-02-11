30a26051aae3b6edc9dc4ee115d47b3febc625b1
Simon Ser <contact@emersion.fr>
2020-02-12 00:29:51 +0100

    contrib/hotreload: be quiet

--- contrib/hotreload.sh
+++ contrib/hotreload.sh
@@ -6,8 +6,8 @@ events=modify,create,delete,move
 targets="themes/ plugins/"
 
 inotifywait -e "$events" -m -r $targets | while read line; do
-	jobs
-	if [ -z "$(jobs -p)" ]; then
+	jobs >/dev/null # Reap status of any terminated job
+	if [ -z "$(jobs)" ]; then
 		(sleep 0.5 && pkill -USR1 koushin) &
 	fi
 done
