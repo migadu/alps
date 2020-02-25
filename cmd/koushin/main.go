3cfd0b942bda7392fd9f26a64251b4b08a9d2a86
Simon Ser <contact@emersion.fr>
2020-02-25 15:14:55 +0100

    cmd: print HTTP requests when -debug is enabled

--- cmd/koushin/main.go
+++ cmd/koushin/main.go
@@ -50,6 +50,11 @@ func main() {
 		e.Logger.Fatal(err)
 	}
 	e.Use(middleware.Recover())
+	if options.Debug {
+		e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+			Format: "${time_rfc3339} method=${method}, uri=${uri}, status=${status}\n",
+		}))
+	}
 
 	if options.Debug {
 		e.Logger.SetLevel(log.DEBUG)
